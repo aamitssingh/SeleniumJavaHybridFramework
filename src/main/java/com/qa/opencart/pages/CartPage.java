515032edacfec76e397d670bf5a4168518e0756e
John Doe <john.doe@omaticsoftware.com>
2022-06-05 09:41:06 -0500

    Added Cart Page

---
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+public class CartPage {
+	
+	public CartPage()
+	{
+		System.out.println("Cart Page Constructor...");
+	}
+	
+	public void addToCart()
+	{
+		System.out.println("Add to Cart Features here");
+	}
+
+}
